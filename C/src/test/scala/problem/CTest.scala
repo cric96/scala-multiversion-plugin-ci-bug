6844e991128d801f0a27e74fd1a01b5a416760d6
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2021-08-28 10:09:25 +0200

    C sleeps more time then A, B

--- C/src/test/scala/problem/CTest.scala
+++ C/src/test/scala/problem/CTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.matchers.should.Matchers
 
 class CTest extends AnyFlatSpec with Matchers{
   it should "work" in {
-    Thread.sleep(1000) // simulate a long test
+    Thread.sleep(5000) // simulate a long test
     C().foo shouldBe A().foo + B().foo
   }
 }
