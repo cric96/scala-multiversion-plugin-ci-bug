b33c052d40347c51243cda2f67c513f45849b660
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2021-08-28 09:55:11 +0200

    add long wait in tests

--- A/src/test/scala/problem/ATest.scala
+++ A/src/test/scala/problem/ATest.scala
@@ -6,6 +6,7 @@ import org.scalatest.matchers.should.Matchers
 class ATest {
   class CTest extends AnyFlatSpec with Matchers{
     it should "work" in {
+      Thread.sleep(1000) // simulate a long test
       A().foo shouldBe "foo"
     }
   }
--- B/src/test/scala/problem/BTest.scala
+++ B/src/test/scala/problem/BTest.scala
@@ -6,6 +6,7 @@ import org.scalatest.matchers.should.Matchers
 class BTest {
   class CTest extends AnyFlatSpec with Matchers{
     it should "work" in {
+      Thread.sleep(1000) // simulate a long test
       B().foo shouldBe A().foo
     }
   }
